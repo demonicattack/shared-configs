dd855ca715a679cad8b057d797efd96fa0673ca6
demonicattack <demonicattacks@outlook.com>
2024-12-12 22:50:15 +0400

    fix(shared): remove unused variable

--- configs/shared/src/transform-keys.ts
+++ configs/shared/src/transform-keys.ts
@@ -1,6 +1,6 @@
 import { typeOf } from './type-of';
 
-const transformKeys = <T extends Record<string, unknown>>(object: T, transform: (s: string) => string): T => {
+const transformKeys = <T extends Record<string, unknown>>(object: T, transform: (_: string) => string): T => {
     if (typeOf(object) !== 'object') return object;
     return Object.keys(object).reduce<T>((accumulator, key) => {
         if (Object.hasOwn(object, key)) {
